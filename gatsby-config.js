adb6e214b58df9d07ee5024e289017f1e4bd7dc2
Mark Conroy <mark@mark.ie>
2020-04-15 21:42:11 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -12,6 +18,51 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: "gatsby-source-google-spreadsheet",
+      options: {
+        // The `spreadsheetId` is required, it is found in the url of your document:
+        // https://docs.google.com/spreadsheets/d/<spreadsheetId>/edit#gid=0
+        spreadsheetId: "1iy4IMH43-aTB2dLOtbYtqVLsLBNC46Z5FJ-Brwl0oSM",
+    
+        // The `spreadsheetName` is recommended, but optional
+        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
+        // If you are sourcing multiple sheets, you can set this to distringuish between the source data
+        spreadsheetName: "Business As Usual (Responses)",
+    
+        // The `typePrefix` is optional, default value is "GoogleSpreadsheet"
+        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
+        // It can be overridden to fully customize the root query
+        typePrefix: "GoogleSpreadsheet",
+    
+        // The `credentials` are only needed when you need to be authenticated to read the document.
+        // It's an object with the following shape:
+        // {
+        //   client_email: "<your service account email address>",
+        //   private_key: "<the prive key for your service account>"
+        // }
+        //
+        // Refer to googles own documentation to retrieve the credentials for your service account:
+        //   - https://github.com/googleapis/google-api-nodejs-client#service-to-service-authentication
+        //   - https://developers.google.com/identity/protocols/OAuth2ServiceAccount
+        //
+        // When you have generated your credentials, it's easiest to refer to them from an environment variable
+        // and parse it directly:
+        credentials: JSON.parse(`${process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS}`),
+    
+        // Simple node transformation during node sourcing can be achieved by implementing the following functions
+        // - `filterNode`
+        // - `mapNode`
+        //
+        // By implementing a `filterNode(node): boolean` function, you can choose to eliminate some nodes before
+        // they're added to Gatsby, the default behaviour is to include all nodes:
+        filterNode: () => true,
+    
+        // By implementing a `mapNode(node): node` function, you can provide your own node transformations directly
+        // during node sourcing, the default implementation is to return the node as is:
+        mapNode: node => node
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
