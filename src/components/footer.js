b5e46337ffdc33512b0bf7740a3ac790fde867dd
Mark Conroy <mark@mark.ie>
2020-04-16 18:27:30 +0100

    adding Grow Remote to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,23 @@
 import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
 
 const StyledFooter = styled.footer`
   border-top: 3px solid var(--primary);
-  .footer__inner {
-    margin: 0 auto;
-    max-width: 960px;
-    padding: 1.5rem 1rem;
-  }
+`
+
+const FooterInner = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.5rem 1rem;
 `
 const Footer = () => (
   <StyledFooter>
-    <div className="footer__inner">
+    <FooterInner>
       <p>
-        © {new Date().getFullYear()}, An initiative of <a href="https://annertech.com">Annertech</a>.
+        © {new Date().getFullYear()}, An initiative of <a href="https://annertech.com">Annertech</a>, in cooperation with <a href="https://growremote.ie">Grow Remote Ireland</a>.
       </p>
       <p>If you'd like to build a version of this for your own country, <a href="https://github.com/markconroy/business-as-usual">the source code is available here</a>.</p>
-    </div>
+    </FooterInner>
   </StyledFooter>
 )
 
