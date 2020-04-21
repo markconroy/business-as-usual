b7331743c49f9c75a9561cd98e70cd7b4065fc26
Mark Conroy <mark@mark.ie>
2020-04-21 09:51:53 +0100

    adding <Container> to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Container from "../components/global-styles/container"
+import StyledHeading from "../components/global-styles/headings.js"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <Link to="/">Wanna go home?</Link>
+    <Container>
+      <StyledHeading className="h2">Page NOT FOUND</StyledHeading>
+      <p>You landed on a page that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">Wanna go home?</Link>
+    </Container>
   </Layout>
 )
 
