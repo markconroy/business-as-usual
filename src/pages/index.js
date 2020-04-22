418491d859c50639e497036c338009a8bf9023b8
Mark Conroy <mark@mark.ie>
2020-04-22 15:04:51 +0100

    move CTAs around on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,20 @@ const IndexPage = () => (
     <SEO />
     
     <Container>
-      <Container narrowContent>
+      <Container paddingNone>
         <StyledHeading>Business As Usual</StyledHeading>
       </Container>
       
       <Container paddingNone>
         <GridContainer>
+        
+          <GridItem>
+            <CTAButton
+              CTAButtonLink="https://docs.google.com/forms/d/e/1FAIpQLSfUgFwGuqAlE3I4scpiOosDwiJntFnA8yatxTbgfBWdxSZZiw/viewform"
+              CTAButtonText="Add Your Business"
+            />
+          </GridItem>
+
           <GridItem>
             <CTAButton
               internalLink
@@ -26,27 +34,11 @@ const IndexPage = () => (
               CTAButtonText="View All Businesses"
             />
           </GridItem>
-        
-          <GridItem>
-            <CTAButton
-              internalLink
-              CTAButtonLink="/businesses/counties"
-              CTAButtonText="Search by County"
-            />
-          </GridItem>
         </GridContainer>
       </Container>
 
-      <Container narrowContent paddingNone>
+      <Container narrowContent paddingNone marginBottom>
         <p>This website is a listing of businesses that are still operating through the COVID-19 Crisis in Ireland and Northern Ireland, brought to you by <a href="https://annertech.com">Annertech</a> and <a href="https://growremote.ie">Grow Remote</a></p>
-        <p>If you want to be included in this directory, please:
-          <Container marginBottom marginTop paddingNone>
-            <CTAButton
-              CTAButtonLink="https://docs.google.com/forms/d/e/1FAIpQLSfUgFwGuqAlE3I4scpiOosDwiJntFnA8yatxTbgfBWdxSZZiw/viewform"
-              CTAButtonText="Add Your Business"
-            />
-          </Container>
-        </p>
       </Container>
     </Container>
 
