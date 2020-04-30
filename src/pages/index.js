b1cc8f74a901f2022f3cf56aed9004a8bee3c57b
Mark Conroy <mark@mark.ie>
2020-04-30 15:33:59 +0100

    remove Link since it's not being used

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import StyledHeading from "../components/global-styles/headings.js"
